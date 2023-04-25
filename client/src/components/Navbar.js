5404173aad5d9db4faa4c5dcae35141cc038e059
unknown <marcuspacc@gmail.com>
2023-04-24 19:04:59 -0700

    created navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,52 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom"
 
-function NavBar({ currentPage, handlePageChange }) {
-    return (
-  
-      <nav class="navbar navbar-expand-lg">
-        {/* <a 
-        className="navbar-brand" 
-        href="#home">
-        </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a 
-              href="#!" 
-              onClick={() => handlePageChange('Home')}
-  
-              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-              >Home</a>
-            </li>
-            <li className="nav-item">
-              <a 
-              href="#exercises" 
-              onClick={() => handlePageChange('Exercises')}
-              className={currentPage === 'Exercises' ? 'nav-link active' : 'nav-link'}
-              >Exercises</a>
-            </li>
-            <li className="nav-item">
-              <a 
-              href="#signup" 
-              onClick={() => handlePageChange('Signup')}
-              className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
-              >Signup</a>
-            </li>
-            <li className="nav-item">
-              <a 
-              href="#login" 
-              onClick={() => handlePageChange('Login')}
-              className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
-              >Login</a>
-            </li>
-          </ul>
-        </div> */}
-      </nav>
-  
-    );
-  }
-  
-  export default NavBar;
+function Navbar() {
+
+  return (
+    <header className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <img src="./InitialsLogo.png" alt="Logo" width="50" height="50" className="d-inline-block align-text-center me-2" />
+          Fitness Application
+        </Link>
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <Link to="/" className="nav-link" aria-current="page">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/exercises" className="nav-link" aria-current="page">Exercises</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/signup" className="nav-link" aria-current="page">Signup</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link" aria-current="page">Login</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
