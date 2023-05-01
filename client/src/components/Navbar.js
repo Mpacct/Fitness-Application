d980eaf27982c5cf563a616d8d95700e8a5e2436
Awal <adrianwalkosz@gmail.com>
2023-04-30 19:52:32 -0700

    removed signup and login button when logged in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/project3-logo.png"
+import Auth from "../utils/auth"
 
 function Navbar() {
+  function handleLogout() {
+    if(Auth.loggedIn()) {
+      return(
+        <Link to="/" className="button btn-outline-dark mt-auto" onClick={() => Auth.logout()} aria-current="page">Logout</Link>
 
+      );
+    } else {
+      return (
+        <Link to="/login" className="button btn-outline-dark mt-auto" aria-current="page">Login</Link>
+
+      );
+    };
+  };
+  function removeSignup() {
+    if(Auth.loggedIn()) {
+      return;
+        
+
+      
+    } else {
+      return (
+        <li className="nav-item">
+              <Link to="/signup" className="nav-link" aria-current="page">Signup</Link>
+            </li>
+
+      );
+    };
+  };
   return (
     <nav className="navbar sticky-top navbar-light bg-light navbar-expand-lg">
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,11 +49,9 @@ function Navbar() {
             <li className="nav-item">
               <Link to="/exercises" className="nav-link" aria-current="page">Exercises</Link>
             </li>
+            {removeSignup()}
             <li className="nav-item">
-              <Link to="/signup" className="nav-link" aria-current="page">Signup</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" className="button btn-outline-dark mt-auto" aria-current="page">Login</Link>
+              {handleLogout()}
             </li>
           </ul>
         </div>
