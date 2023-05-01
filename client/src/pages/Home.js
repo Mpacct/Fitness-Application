2002b5098645ca674ff295f76f24564b26db7656
unknown <marcuspacc@gmail.com>
2023-04-30 20:51:35 -0700

    styled pages based off of logged in or logged out

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,36 @@
 import React from "react";
 import Exercises from "./Exercises";
 import Dashboard from "../components/Dashboard";
+import '../assets/card.css';
+import Auth from "../utils/auth"
+import { Link } from "react-router-dom";
+
+const loginLink = <Link to="/login">Login</Link>
+const signupLink = <Link to="/signup">Signup</Link>
+
 function Home() {
+
+    function handleDashboard() {
+        if (!Auth.loggedIn()) {
+            return(
+                <h2 className="text-center m-5">Please {loginLink} or {signupLink} to View your Dashboard</h2>
+            )
+        } else {
+
+            return (
+                <div>
+                    <h2 className="text-center m-5">Dashboard</h2>
+                    <Dashboard />
+                    <h2 className="text-center mt-5">Saved Exercises</h2>
+                    <Exercises />
+                </div>
+            )
+        }
+    }
     return (
         <div>
-            <Dashboard />
-            <Exercises />
+        {handleDashboard()}
         </div>
     )
 }
-
 export default Home;
